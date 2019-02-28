6ea5a29c3071135f9ab726531db5ad26fcc3c1b5
icarmon <carmon.iris@gmail.com>
2019-02-28 20:58:44 +0200

    divisions to H and V and output to file

--- hashcode/ParseInput.java
+++ hashcode/ParseInput.java
@@ -1,22 +1,24 @@
 package hashcode;
 
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.chrono.MinguoChronology;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ParseInput {
 
 	static int N;
 	static SpecialImage[] allPhotos;
-	
+	static List<List<Integer>> solution = new ArrayList<>();
+	static Map<Integer, SpecialImage> verticals = new HashMap<>();
+	static Map<Integer, SpecialImage> horizontals = new HashMap<>();
+
 	public static void main(String[] args) {
 		String pathDir = "C:/Users/Iris/Desktop/hashcode/";
 		String fileNameA = "a_example.txt";
@@ -27,27 +29,37 @@ public class ParseInput {
 		File file = new File(pathDir + fileNameB);
 		parseFile(file);
 		printImages();
+		
+		
 	}
 
 
 	public static void parseFile(File file) {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(file));
-			
+
 			N = Integer.parseInt(reader.readLine());
 			allPhotos = new SpecialImage[N];
-			
+
 			for (int i = 0; i < N; i++) {
 				String s = reader.readLine();
 				String[] ss = s.split(" ");
 				List<String> tags = new ArrayList<>();
-				
-				allPhotos[i] = new SpecialImage(ss[0].charAt(0), tags, i);
+
+				SpecialImage image = new SpecialImage(ss[0].charAt(0), tags, i);
+				allPhotos[i] = image;
 				int numOfTags = Integer.parseInt(ss[1]);
 				for (int j = 2; j < numOfTags+2; j++) {
 					tags.add(ss[j]);
 				}
 				
+				if (image.imgType == 'H') {
+					horizontals.put(image.id, image);
+				}
+				else {
+					verticals.put(image.id, image);
+				}
+
 			}
 
 			reader.close();
@@ -56,30 +68,33 @@ public class ParseInput {
 			e.printStackTrace();
 		} 
 	}
-	
-	public static void createOutput(int[][] solution) {
+
+	public static void createOutput() {
 		String pathDir = "C:/Users/Iris/Desktop/hashcode/";
 		String fileName = "solution";
-		File file = new File(pathDir + fileName);
+		String problemSymbol = "B";
+		File file = new File(pathDir + fileName + problemSymbol + ".txt");
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-			
-			writer.write(solution.length);
-			for (int i = 0; i < solution.length; i++) {
-				for (int j = 0; j < solution[1].length; j++) {
-					writer.write(solution[i][j]);
+
+			writer.write(solution.size());
+			for (int i = 0; i < solution.size(); i++) {
+				for (Integer pic : solution.get(i)) {
+					writer.write(pic + " ");
+//					System.out.print(pic + " ");
 				}
 				writer.newLine();
+//				System.out.println();
 			}
 			writer.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
+
+
+
+
 	}
 
 	public static void arrPrint(char[][] arr) {
@@ -90,7 +105,7 @@ public class ParseInput {
 			System.out.println();
 		}
 	}
-	
+
 	public static void printImages() {
 		for (int i = 0; i < N; i++) {
 			System.out.print("image " +  i + ": ");
